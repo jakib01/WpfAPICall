ddb52453e99066b1e7556220f16b20bb1193aba3
Julker Nien Akib <julkernienakib@gmail.com>
2022-12-07 22:39:56 +0600

    first commit

---
+++ WpfAPICall/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAPICall.Models;
+
+namespace WpfAPICall
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://dummy.restapiexample.com/api/v1/employees");
+                var status = response.EnsureSuccessStatusCode();
+
+                //var dataJson = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    var dataJson = JsonConvert.DeserializeObject<List<Employee>>(await response.Content.ReadAsStringAsync());
+                    //string json = JsonConvert.SerializeObject(_data.ToArray());
+
+                    //write string to file
+                    System.IO.File.WriteAllText(@"D:\path.txt", await response.Content.ReadAsStringAsync());
+                    //massage.Content = "Success";
+                }
+                else
+                {
+                    //massage.Content = $"Server error code{response.StatusCode}";
+                }
+            }
+        }
+
+
+        private void Generator_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+
+}
---
+++ WpfAPICall/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAPICall.Models
+{
+    public class employee
+    {
+        public List<Employee> Employee { get; set; }
+    }
+
+    public class Employee
+    {
+        //public int Id { get; set; }
+        //public string? Name { get; set; }
+        //public string? Salary { get; set; }
+        //public string? Age { get; set; }
+
+
+        public int Id { get; set; }
+        public string? employee_name { get; set; }
+        public string? employee_salary { get; set; }
+        public string? employee_age { get; set; }
+        public string? profile_image { get; set; }
+
+    }
+}

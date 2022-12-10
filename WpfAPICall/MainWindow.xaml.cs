5f0a3908d80cfa68c9fb334c5a486474bda05ed5
Julker Nien Akib <julkernienakib@gmail.com>
2022-12-10 19:56:52 +0600

    two table view

--- WpfAPICall/MainWindow.xaml.cs
+++ WpfAPICall/MainWindow.xaml.cs
@@ -21,7 +21,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WpfAPICall.Models;
 using Path = System.IO.Path;
 
 namespace WpfAPICall
